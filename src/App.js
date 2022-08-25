e7e5bef4e7699fa74a2ec4c2dc1b2a5e777acd1f
Raphael Pacheco <raphaelrogen@gmail.com>
2022-08-25 15:37:15 -0300

    requisito 05 concluido

--- src/App.js
+++ src/App.js
@@ -14,7 +14,32 @@ class App extends React.Component {
     cardTrunfo: false,
     /* hasTrunfo: true, */
     isSaveButtonDisabled: true,
+  };
 
+  saveForm = () => {
+    const { cardName,
+      cardImage,
+      cardDescription,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+    const maxAtr = 91;
+    const minAtr = 0;
+    const sun = (Number(cardAttr1)) + (Number(cardAttr2)) + (Number(cardAttr3));
+    const num = 210;
+    if (cardName && cardDescription && cardImage && cardRare
+      && cardAttr1 < maxAtr && cardAttr2 < maxAtr && cardAttr3 < maxAtr
+      && cardAttr1 >= minAtr && cardAttr2 >= minAtr && cardAttr3 >= minAtr
+      && sun <= num) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   };
 
   onInputChange = ({ target }) => {
@@ -22,7 +47,7 @@ class App extends React.Component {
     const value = type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, () => console.log(this.saveForm()));
   };
 
   render() {
@@ -41,7 +66,18 @@ class App extends React.Component {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form onInputChange={ this.onInputChange } />
+        <Form
+          onInputChange={ this.onInputChange }
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardImage={ cardImage }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+        />
         <Card
           cardName={ cardName }
           cardDescription={ cardDescription }
